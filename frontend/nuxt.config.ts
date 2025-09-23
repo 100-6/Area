73aee9d11f49626a33069fdae9b28b262f8fe15b
ouioui <adam.larkem@epitech.eu>
2025-09-23 15:17:07 +0200

    feat: (FRONTEND) tailwind

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -2,11 +2,14 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   devServer: {
-    port: parseInt(process.env.NUXT_PORT),
+    port: parseInt(process.env.NUXT_PORT || '3000'),
     host: '0.0.0.0'
   },
+  modules: [
+    '@nuxtjs/tailwindcss'
+  ],
   css: [
-    '~/style/main.css', 
+    '~/style/main.css',
     '~/style/variables.css'
   ]
 })
