65332232c2da246dd15cc40a738d081505d530e0
Bastian130 <bastian.carou@epitech.eu>
2025-10-14 11:36:10 +0200

    fix(workflowController): useless parameters remove

--- backend/src/core/controllers/WorkflowController.ts
+++ backend/src/core/controllers/WorkflowController.ts
@@ -145,13 +145,12 @@ export class WorkflowController {
         }
         try {
             // Vérifier si on doit éviter de redémarrer les triggers (lors de la modification d'un workflow)
-            const skipTriggerStart = req.query.skipTriggerStart === 'true' || req.headers['x-skip-trigger-start'] === 'true';
 
             const connection = await this.workflowService.createConnection(areaId, {
                 sourceNodeId,
                 targetNodeId,
                 condition
-            }, skipTriggerStart);
+            });
 
             res.status(201).json({
                 success: true,
