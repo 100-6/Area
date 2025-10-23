51fc0be0617894ea0e20abf4bb128761fa2abd19
dtakougnadiepi <dylan.takougnadi@epitech.eu>
2025-10-23 12:54:02 +0200

    feat(dropbox): implement 5 file management actions
    Add essential Dropbox actions for workflow automation:
    - upload_file: Create text files with dynamic content
    - create_shared_link: Generate public sharing links
    - delete_file: Remove files/folders
    - move_file: Move or rename files
    - copy_file: Create file copies/backups

---
+++ backend/src/modules/dropbox/actions/_index.ts
@@ -0,0 +1,6 @@
+export { UploadFileAction } from './UploadFile';
+export { CreateSharedLinkAction } from './CreateSharedLink';
+export { DeleteFileAction } from './DeleteFile';
+export { MoveFileAction } from './MoveFile';
+export { CopyFileAction } from './CopyFile';
+
