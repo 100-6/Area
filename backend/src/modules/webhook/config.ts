941f9f50dfc3d259f231015f857a4709b16ca1d5
Bastian Carou <bastian.carou@epitech.eu>
2025-11-02 18:51:09 +0100

    Update webhook icon URL in config

--- backend/src/modules/webhook/config.ts
+++ backend/src/modules/webhook/config.ts
@@ -8,7 +8,7 @@ export default {
     name: 'webhook',
     displayName: 'Webhook',
     description: 'Receive HTTP POST webhooks and trigger workflows',
-    iconUrl: 'https://i.sstatic.net/S3SNU.jpg',
+    iconUrl: 'https://cdn.iconscout.com/icon/free/png-256/free-webhooks-icon-svg-download-png-282425.png',
     color: '#c4c4c4',
     authType: 'none',
     isActive: true,
